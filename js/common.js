65079f87d9c79e3bfd7a14bf3395ffb8ea49ff9e
Ethan Nelson <git@ethan-nelson.com>
2017-03-13 20:54:10 -0500

    Consolidate radar refresh

--- js/common.js
+++ js/common.js
@@ -4,7 +4,7 @@ var imgPrefix = "{{ site.baseurl }}{{ site.imagedir }}";
 
 {% if site.attribution %}
 function attribution (){
-  window.alert("{{ site.attribution }}");
+    window.alert("{{ site.attribution }}");
 }
 {% else %}
 function attribution () {};
@@ -13,3 +13,11 @@ function attribution () {};
 var mapimg = L.tileLayer('{{ site.mapdir }}', {maxZoom: 12, minZoom: 6, attribution: '<a href="javascript: attribution()">Map Attributions</a>'});
 
 var radarimg = L.imageOverlay('{{ site.radarimg }}', {{ site.radarbounds }}, {opacity: 0.5});
+
+function reloadRadar() {
+    map.removeLayer(radarimg);
+    image = '{{ site.radarimg }}?lastmod' + new Date().getTime();
+    radarimg = new L.ImageOverlay(image, {{ site.radarbounds }}, {opacity: 0.5});
+    map.addLayer(radarimg);
+    radarimg.bringToBack();
+}
