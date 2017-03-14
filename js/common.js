b44072ac75b87ca7c8c0445e4a08426b9d3edc6d
Ethan Nelson <git@ethan-nelson.com>
2017-03-13 19:24:27 -0500

    Move map to common.js

--- js/common.js
+++ js/common.js
@@ -1,4 +1,13 @@
 ---
 ---
-var imgPrefix = "{{ site.baseurl }}/images/";
+var imgPrefix = "{{ site.baseurl }}{{ site.imagedir }}";
 
+{% if site.attribution %}
+function attribution (){
+  window.alert("{{ site.attribution }}");
+}
+{% else %}
+function attribution () {};
+{% endif %}
+
+var mapimg = L.tileLayer('{{ site.mapdir }}', {maxZoom: 12, minZoom: 6, attribution: '<a href="javascript: attribution()">Map Attributions</a>'});
