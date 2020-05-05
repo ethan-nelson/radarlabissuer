aca747855b8442aac1412704583b5a2b523d7e72
Ethan Nelson <git@ethan-nelson.com>
2020-05-04 21:09:27 -0500

    work on radar viewer

--- js/common.js
+++ js/common.js
@@ -1,6 +1,81 @@
 ---
 ---
+// Pad time strings with a leading zero.
+function checkTime(i) {
+    if (i<10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
+
+
+var classyear = 2020;
+var classmonth = 4;
+var classday = 27;
+var classdayone = classday + 1;
+
+var radarTimes = {
+    '233506': new Date(checkTime(classyear.toString())+"-"+checkTime(classmonth.toString())+"-"+checkTime(classday.toString())+"T23:35:06Z"),
+    '234130': new Date(checkTime(classyear.toString())+"-"+checkTime(classmonth.toString())+"-"+checkTime(classday.toString())+"T23:41:30Z"),
+    '234827': new Date(checkTime(classyear.toString())+"-"+checkTime(classmonth.toString())+"-"+checkTime(classday.toString())+"T23:48:27Z"),
+    '235524': new Date(checkTime(classyear.toString())+"-"+checkTime(classmonth.toString())+"-"+checkTime(classday.toString())+"T23:55:24Z"),
+    '000221': new Date(checkTime(classyear.toString())+"-"+checkTime(classmonth.toString())+"-"+checkTime(classdayone.toString())+"T00:02:21Z"),
+    '000853': new Date(checkTime(classyear.toString())+"-"+checkTime(classmonth.toString())+"-"+checkTime(classdayone.toString())+"T00:11:53Z"),
+    '001527': new Date(checkTime(classyear.toString())+"-"+checkTime(classmonth.toString())+"-"+checkTime(classdayone.toString())+"T00:22:27Z"),
+    '002153': new Date(checkTime(classyear.toString())+"-"+checkTime(classmonth.toString())+"-"+checkTime(classdayone.toString())+"T00:33:53Z"),
+    '002830': new Date(checkTime(classyear.toString())+"-"+checkTime(classmonth.toString())+"-"+checkTime(classdayone.toString())+"T00:44:30Z"),
+    '003453': new Date(checkTime(classyear.toString())+"-"+checkTime(classmonth.toString())+"-"+checkTime(classdayone.toString())+"T00:55:53Z"),
+    '004137': new Date(checkTime(classyear.toString())+"-"+checkTime(classmonth.toString())+"-"+checkTime(classdayone.toString())+"T01:06:37Z"),
+    '004833': new Date(checkTime(classyear.toString())+"-"+checkTime(classmonth.toString())+"-"+checkTime(classdayone.toString())+"T01:17:33Z"),
+    '005520': new Date(checkTime(classyear.toString())+"-"+checkTime(classmonth.toString())+"-"+checkTime(classdayone.toString())+"T01:28:20Z"),
+    '010215': new Date(checkTime(classyear.toString())+"-"+checkTime(classmonth.toString())+"-"+checkTime(classdayone.toString())+"T01:39:15Z"),
+    '010910': new Date(checkTime(classyear.toString())+"-"+checkTime(classmonth.toString())+"-"+checkTime(classdayone.toString())+"T01:50:10Z")
+};
+
+var radarKeys = [];
+for (var key in radarTimes) {
+    radarKeys.push(key);
+}
+
+var hourOffset = 7;
+var minuteOffset = 45;
+var availableTimes = radarKeys.slice(0,4);
+var sliderTimes = [];
+var latestTime = radarKeys[0];
+
 var imgPrefix = "{{ site.baseurl }}{{ site.imagedir }}";
+
+// File path construction
+function getFile(product, tilt, time) {
+    return '{{ site.baseurl }}/{{ site.imagedir }}/radar/' + product + '/' + time + '_' + tilt + '_' + product + '.png'
+}
+
+// Check valid radar times compared to now
+function validateTimes() {
+    fakeNow = new Date();
+    fakeNow.setTime(fakeNow.getTime() + (hourOffset*3600*1000) + (minuteOffset*60*1000));
+    availableTimes = radarKeys.slice(0,4);
+    for (var i=4; i<radarKeys.length; i++) {
+        if (fakeNow > radarTimes[radarKeys[i]]) {
+            availableTimes.push(radarKeys[i]);
+        }
+    }
+}
+validateTimes();
+
+// Get the latest time that has elapsed in fake time
+function getLatestTime() {
+    fakeNow = new Date();
+    fakeNow.setTime(fakeNow.getTime() + (hourOffset*3600*1000) + (minuteOffset*60*1000));
+    latestTime = radarKeys[0];
+    for (var i=0; i<radarKeys.length; i++) {
+        if (fakeNow > radarTimes[radarKeys[i]]) {
+            latestTime = radarKeys[i];
+        } else {
+            break;
+	}
+    }
+}
+getLatestTime();
+
 
 {% if site.attribution %}
 function attribution (){
@@ -13,37 +88,24 @@ function attribution () {};
 var mapimg = L.tileLayer('{{ site.mapdir }}', {maxZoom: 12, minZoom: 6, attribution: '<a href="javascript: attribution()">Map Attributions</a>'});
 var mapimg2 = L.tileLayer('{{ site.mapdir }}', {maxZoom: 12, minZoom: 6, attribution: '<a href="javascript: attribution()">Map Attributions</a>'});
 
-var radarimg = L.imageOverlay('{{ site.radarimg }}', {{ site.radarbounds }}, {opacity: 0.5});
+var radarimg = L.imageOverlay(getFile('reflectivity', '0.5', latestTime), {{ site.radarbounds }}, {opacity: 0.5});
 
 function reloadRadar() {
     map.removeLayer(radarimg);
-    image = '{{ site.radarimg }}?lastmod' + new Date().getTime();
+    getLatestTime();
+    image = getFile('reflectivity', '0.5', latestTime);
     radarimg = new L.ImageOverlay(image, {{ site.radarbounds }}, {opacity: 0.5});
     map.addLayer(radarimg);
     radarimg.bringToBack();
 }
 
 
-// Pad time strings with a leading zero.
-function checkTime(i) {
-    if (i<10) {i = "0" + i};  // add zero in front of numbers < 10
-    return i;
-}
-
 
 // Get the simulation time and display it on the page.
 function getTime() {
     var today=new Date();
-    today.setHours(today.getHours() + {{ site.houroffset }});
-    today.setMinutes(today.getMinutes() + {{ site.minuteoffset }});
-    var h=today.getHours();
-    var m=today.getMinutes();
-    var s=today.getSeconds();
-    h = checkTime(h);
-    m = checkTime(m);
-    s = checkTime(s);
-    document.getElementById('addTime').innerHTML = "Simulation Time: " + h+":"+m+":"+s;
-    document.getElementById('radartime').value = h+":"+m;
+    today.setTime(today.getTime() + (hourOffset*3600*1000) + (minuteOffset*60*1000));
+    document.getElementById('addTime').innerHTML = "Simulation Time: " + today.toLocaleTimeString('en-US', {timeZone: 'UTC'});
     var t = setTimeout(function(){getTime()},500);
 }
 
--- js/radarview.js
+++ js/radarview.js
@@ -1,5 +1,59 @@
 ---
 ---
+
+var delayedTimes = {
+'233506': '233506',
+'234130': '234130',
+'234827': '234827',
+'235524': '235524',
+'000221': '000221',
+'000853': '001153',
+'001527': '002227',
+'002153': '003353',
+'002830': '004430',
+'003453': '005553',
+'004137': '010637',
+'004833': '011733',
+'005520': '012820',
+'010215': '013915',
+'010910': '015010',
+};
+
+var angles = [
+'0.5',
+'2.4',
+'5.1',
+'6.4',
+'12.5'
+];
+
+// Update the time sliders when new radar comes in
+function updateTimeSliders() {
+    if (JSON.stringify(availableTimes) != JSON.stringify(sliderTimes)) {
+        console.log('updating sliders');
+        for (var key in times) {
+            times[key].setAttribute('max', availableTimes.length-1);
+            times[key].value = availableTimes.length - 1;
+        }
+        switchImage('map1');
+        switchImage('map2');
+        console.log('updating radar images--new ones available!');
+        sliderTimes = availableTimes;
+    }
+}
+
+
+function reloadLayers() {
+    validateTimes();
+    updateTimeSliders();
+}
+
+
+// Refresh the layers forever
+function refreshForever() {
+    setInterval(function() {reloadLayers()}, 15000);
+}
+
 var times = {'time1': document.getElementById('time1'),
              'time2': document.getElementById('time2')};
 var tilts = {'tilt1': document.getElementById('tilt1'),
@@ -35,15 +89,28 @@ var synctime = document.getElementById('synctime');
 function syncTime(current, other) {
     if (synctime.checked === true) {
         times[other].value = times[current].value;
+        switchImage(other);
     }
 }
+
 
 // Sync tilts
 var synctilt = document.getElementById('synctilt');
 function syncTilt(current, other) {
     if (synctilt.checked === true) {
         tilts[other].value = tilts[current].value;
+        switchImage(other);
     }
+}
+
+// Get time
+function getSelectedTime(e) {
+    return availableTimes[times[e].value];
+}
+
+// Get tilt
+function getSelectedTilt(e) {
+    return angles[tilts[e].value];
 }
 
 // Sync pan
@@ -66,37 +133,56 @@ function follow(e) {
     }
 }
 
-// Image retrieval
-function getFile(product, tilt, time) {
-  return '/radarlabissuer/images/radar/' + product + '/' + time + '_' + product + '.png'
+
+function switchImage(inmap) {
+    if (inmap === 'map1') {
+        map1.removeLayer(map1Layer);
+        map1Layer = new L.ImageOverlay(getFile(map1Product, getSelectedTilt('tilt1'), getSelectedTime('time1')), {{ site.radarbounds }}, {opacity: 0.5});
+        map1Layer.addTo(map1);
+        var thetimehere = delayedTimes[getSelectedTime('time1')];
+        document.getElementById('map1Time').innerHTML = 'Displayed Radar Tilt ' + getSelectedTilt('tilt1') + '&#176;, Time ' + thetimehere.slice(0,2) + ':' + thetimehere.slice(2,4) + 'Z';
+    } else {
+        map2.removeLayer(map2Layer);
+        map2Layer = new L.ImageOverlay(getFile(map2Product, getSelectedTilt('tilt2'), getSelectedTime('time2')), {{ site.radarbounds }}, {opacity: 0.5});
+        map2Layer.addTo(map2);
+        var thetimehere = delayedTimes[getSelectedTime('time2')];
+        document.getElementById('map2Time').innerHTML = 'Displayed Radar Tilt ' + getSelectedTilt('tilt2') + '&#176;, Time ' + thetimehere.slice(0,2) + ':' + thetimehere.slice(2,4) + 'Z';
+    }
 }
 
+var map1Product = '';
+var map2Product = '';
 // Product selection
 function initializeProducts() {
-    map1Layer = new L.ImageOverlay(getFile('reflectivity', '0.0', 'latest'), {{ site.radarbounds }}, {opacity: 0.5});
+    map1Layer = new L.ImageOverlay(getFile('reflectivity', '0.5', 'latest'), {{ site.radarbounds }}, {opacity: 0.5});
+    map1Product = 'reflectivity';
     map1Layer.addTo(map1);
-    map2Layer = new L.ImageOverlay(getFile('velocity', '0.0', 'latest'), {{ site.radarbounds }}, {opacity: 0.5});
+    map1Buttons[0].classList.add('active');
+    map2Layer = new L.ImageOverlay(getFile('velocity', '0.5', 'latest'), {{ site.radarbounds }}, {opacity: 0.5});
+    map2Product = 'velocity';
     map2Layer.addTo(map2);
+    map2Buttons[1].classList.add('active');
 }
 
-function switchProduct(inmap, product, tilt, time) {
+function switchProduct(inmap, product) {
     if (inmap === 'map1') {
         map1.removeLayer(map1Layer);
         for (var i = 0; i < 4; i++) {
             map1Buttons[i].classList.remove('active');
         }
-        map1Layer = new L.ImageOverlay(getFile(product, tilt, time), {{ site.radarbounds }}, {opacity: 0.5});
+        map1Layer = new L.ImageOverlay(getFile(product, getSelectedTilt('tilt1'), getSelectedTime('time1')), {{ site.radarbounds }}, {opacity: 0.5});
+        map1Product = product;
         map1.addLayer(map1Layer);
     } else {
         map2.removeLayer(map2Layer);
         for (var i=0; i < 4; i++) {
             map2Buttons[i].classList.remove('active');
         }
-        map2Layer = new L.ImageOverlay(getFile(product, tilt, time), {{ site.radarbounds }}, {opacity: 0.5});
+        map2Layer = new L.ImageOverlay(getFile(product, getSelectedTilt('tilt2'), getSelectedTime('time2')), {{ site.radarbounds }}, {opacity: 0.5});
+        map2Product = product;
         map2.addLayer(map2Layer);
     }
 }
-
 
 var map1 = L.map('map1', {layers: [mapimg]}).setView({{ site.mapcenter}}, {{ site.mapzoom }});
 
@@ -106,3 +192,7 @@ map1.on('moveend', follow).on('zoomend', follow);
 map2.on('moveend', follow).on('zoomend', follow);
 
 initializeProducts();
+
+window.onload = getTime();
+reloadLayers();
+refreshForever();
